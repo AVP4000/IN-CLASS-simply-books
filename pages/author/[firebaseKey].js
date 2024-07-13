08485571fa3226ec57c1be7431f363f49cdbede6
Andre Phosarath <andrepho22@gmail.com>
2024-07-13 00:18:38 -0500

    added view author details , still need do merged data

---
+++ pages/author/[firebaseKey].js
@@ -0,0 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { viewAuthorDetails } from '../../api/mergedData';
+import BookCard from '../../components/BookCard';
+
+export default function Viewauthor() {
+  const [authorDetails, setAuthorDetails] = useState({});
+  const router = useRouter();
+
+  // TODO: grab firebaseKey from url
+  const { firebaseKey } = router.query;
+
+  const getTheAuthorDetails = () => {
+    viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+  };
+
+  // TODO: make call to API layer to get the data
+  useEffect(() => {
+    viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+  }, [firebaseKey]);
+
+  return (
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="text-white ms-5 details">
+        <h5>
+          {authorDetails.first_name} {authorDetails.last_name}
+          {authorDetails.favorite ? ' 🤍' : ''}
+        </h5>
+        <a href={`mailto:${authorDetails.authorObject?.email}`}>{authorDetails.authorObject?.email}</a>
+        <div>
+          {authorDetails.books?.map((book) => (
+            <BookCard key={book.firebaseKey} bookObj={book} onUpdate={getTheAuthorDetails} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
